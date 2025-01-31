7a365502b3045974fc1f922ec39b3568cfa41923
MohammadAmin HajiAlirezaei <m.a.hajialirezaei05@gmail.com>
2025-01-31 17:37:50 +0330

    Update code_Question1.py

--- code_Question1.py
+++ code_Question1.py
@@ -12,9 +12,9 @@ square_wave = amplitude * np.sign(np.sin(2 * np.pi * frequency * t))
 
 plt.figure(figsize=(10, 4))
 plt.plot(t, square_wave)
-plt.title('سیگنال مربعی')
-plt.xlabel('زمان (ثانیه)')
-plt.ylabel('دامنه')
+plt.title('Square Wave Signal')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Amplitude')
 plt.grid(True)
 plt.show()
 
@@ -23,14 +23,12 @@ frequencies = np.fft.fftfreq(len(square_wave), 1/sampling_rate)
 
 plt.figure(figsize=(10, 4))
 plt.plot(frequencies, np.abs(fft_result))
-plt.title('طیف فرکانسی (DFT)')
-plt.xlabel('فرکانس (هرتز)')
-plt.ylabel('دامنه')
+plt.title('Frequency Spectrum (DFT)')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
 plt.xlim(0, 500)
 plt.grid(True)
 plt.show()
-
-
 
 num_harmonics = 20
 fourier_coeffs = []
@@ -44,27 +42,22 @@ for n in range(1, num_harmonics + 1):
 plt.figure(figsize=(10, 4))
 n_values = np.arange(1, num_harmonics + 1)
 plt.stem(n_values, fourier_coeffs)
-plt.title('ضرایب سری فوریه')
-plt.xlabel('n (شماره هارمونیک)')
-plt.ylabel('دامنه')
+plt.title('Fourier Series Coefficients')
+plt.xlabel('n (Harmonic Number)')
+plt.ylabel('Amplitude')
 plt.grid(True)
 plt.show()
-
-
-
 
 reconstructed_signal = np.zeros_like(t)
 for n in range(1, num_harmonics + 1, 2):
     reconstructed_signal += (4 * amplitude / (n * np.pi)) * np.sin(2 * np.pi * n * frequency * t)
 
 plt.figure(figsize=(10, 4))
-plt.plot(t, square_wave, label='سیگنال اصلی')
-plt.plot(t, reconstructed_signal, label='سیگنال بازسازی شده')
-plt.title('بازسازی سیگنال با استفاده از سری فوریه')
-plt.xlabel('زمان (ثانیه)')
-plt.ylabel('دامنه')
+plt.plot(t, square_wave, label='Original Signal')
+plt.plot(t, reconstructed_signal, label='Reconstructed Signal')
+plt.title('Signal Reconstruction using Fourier Series')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Amplitude')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-
